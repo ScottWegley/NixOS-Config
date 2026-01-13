84ac3736faca3fbcfd80294b6d7c552579db9124
Scott Wegley <scbrwegley@pm.me>
2026-01-12 19:03:25 -0700

    smh

--- nixos/core/bootloader.nix
+++ nixos/core/bootloader.nix
@@ -1,12 +1,18 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 
 {
   imports = [
     inputs.lanzaboote.nixosModules.lanzaboote
   ];
 
-  # boot.lanzaboote = {
-  #   enable = true;
-  #   pkiBundle = "/var/lib/sbctl";
-  # };
+  # Lanzaboote currently replaces the systemd-boot module.
+  # This setting is usually set to true in configuration.nix
+  # generated at installation time. So we force it to false
+  # for now.
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/var/lib/sbctl";
+  };
 }
