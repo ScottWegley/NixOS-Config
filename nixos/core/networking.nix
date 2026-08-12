967e0a3d306a2ccc683f5b37c76a97b0a4ce2b45
Scott Wegley <scbrwegley@pm.me>
2026-08-11 23:07:57 -0700

    rdp attempt

--- nixos/core/networking.nix
+++ nixos/core/networking.nix
@@ -4,9 +4,10 @@
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [
-      8080
+      8080 # http https
       80
       443
+      3389 # rdp
     ];
   };
 
--- nixos/core/rdp.nix
+++ nixos/core/rdp.nix
@@ -1,18 +1,12 @@
 {pkgs, ...}: {
-  # Enable xrdp
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session";
-  services.xrdp.openFirewall = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
-  # GNOME remote desktop backend
   services.gnome.gnome-remote-desktop.enable = true;
 
-  # Disable autologin to avoid session conflicts
-  services.displayManager.autoLogin.enable = false;
-
-  # Disable sleep/hibernate so remote sessions aren't interrupted
-  systemd.targets.sleep.enable = false;
-  systemd.targets.suspend.enable = false;
-  systemd.targets.hibernate.enable = false;
-  systemd.targets.hybrid-sleep.enable = false;
+  systemd.services.gnome-remote-desktop = {
+    wantedBy = ["graphical.target"];
+    after = ["network.target" "gdm.service"];
+    wants = ["network.target" "gdm.service"];
+  };
 }
